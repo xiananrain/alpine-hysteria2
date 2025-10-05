997113508699acff94229a5b4ddbe2fff379c7b0
xianan <101491184+xiananrain@users.noreply.github.com>
2025-10-05 22:51:33 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -31,7 +31,7 @@ auth:
 masquerade:
   type: proxy
   proxy:
-    url: https://bing.com/
+    url: https://iosapps.itunes.apple.com/
     rewriteHost: true
 EOF
 }
@@ -80,7 +80,7 @@ service hysteria start
 
 echo "------------------------------------------------------------------------"
 echo "hysteria2已经安装完成"
-echo "默认端口： 40443 ， 密码为： $GENPASS ，工具中配置：tls，SNI为： bing.com"
+echo "默认端口： 40443 ， 密码为： $GENPASS ，工具中配置：tls，SNI为： iosapps.itunes.apple.com"
 echo "配置文件：/etc/hysteria/config.yaml"
 echo "已经随系统自动启动"
 echo "看状态 service hysteria status"
